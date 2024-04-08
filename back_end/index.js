c46f8b591b0b5580261687c33725513d2d9db369
ArshdeepSingh <146491831+iArshdeep-Singh@users.noreply.github.com>
2024-04-08 08:43:28 +0530

    last one commit

--- back_end/index.js
+++ back_end/index.js
@@ -18,7 +18,11 @@ const urlx = process.env.URLX
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}))
 app.use(morgan('dev'))
 
 

4a17cf7eefbaf664a5753c94d2bba028c7945b0d
ArshdeepSingh <146491831+iArshdeep-Singh@users.noreply.github.com>
2024-04-08 09:07:09 +0530

    Update deleteSlice.js

--- front_end/src/store/slices/deleteSlice.js
+++ front_end/src/store/slices/deleteSlice.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const deleteAPI = createAsyncThunk("deleteAPI", async ({token, password}, {rejectWithValue}) => {
     try
     {
-        const res = await axios.delete(`http://127.0.0.1:2000/user/${token}`, {
+        const res = await axios.delete(`news-website-nine-virid.vercel.app/user/${token}`, {
             data: {password}
         })
 

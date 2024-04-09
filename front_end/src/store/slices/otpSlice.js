7e8a984fc125a6810516f1cb99ca831fd9cf636a
ArshdeepSingh <146491831+iArshdeep-Singh@users.noreply.github.com>
2024-04-09 13:36:37 +0530

    Update otpSlice.js

--- front_end/src/store/slices/otpSlice.js
+++ front_end/src/store/slices/otpSlice.js
@@ -4,7 +4,7 @@ import axios from "axios"
 export const otpAPI = createAsyncThunk('otpAPI', async ({otp, token}, {rejectWithValue}) => {
     try
     {
-        const res = await axios.post(`http://127.0.0.1:2000/user/verify-otp/${token}`, {otp})
+        const res = await axios.post(`https://news-website-sable.vercel.app/user/verify-otp/${token}`, {otp})
 
         return res.data
     } catch (error)
